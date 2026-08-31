1e11169c235894a4c3dd47464905844185abd61e
DilipPalanisamy <dilippalanisamy09@gmail.com>
2026-08-31 23:00:48 +0530

    Remove all contents from repository

--- src/lib/firebase.ts
+++
@@ -1,6 +0,0 @@
-/**
- * FIREBASE COMPLETELY REMOVED.
- * Authentication is powered directly by Google Identity Services (GIS) / Google OAuth 2.0.
- * See: src/lib/googleAuth.ts
- */
-export const isFirebaseRemoved = true;
